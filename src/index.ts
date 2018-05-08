fd52ba573a30ae54caa6aba9008db891c6112c24
Gislainy Crisostomo Velasco <gislainycrisostomo@gmail.com>
2018-05-08 12:54:16 -0300

    alterado o index

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
 import { NgModule,  } from '@angular/core';
 import { PodeComponent } from './pode/pode';
 
+import { IonicPageModule } from 'ionic-angular';
 const components = [PodeComponent]
 @NgModule({
 	declarations: components,
-	imports: components,
+	imports: [
+		components,
+		IonicPageModule.forChild(PodeComponent),
+	],
 	exports: components,
   entryComponents: components
 })
-export class ComponentsModule { }
+export class RolesComponentModules { }
